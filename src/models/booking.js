41af2a9e423d67354a680d8e5032ec2f7d1bb4a5
anmol_dhama <anmol.dhama@rapidshyp.com>
2025-02-03 17:21:45 +0530

    added booking migration

--- src/models/booking.js
+++ src/models/booking.js
@@ -28,6 +28,16 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
       defaultValue: 'InProcess'
     },
+    noOfSeats: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 1
+    },
+    totalCost:{
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0
+    }
   }, {
     sequelize,
     modelName: 'Booking',
